fc9aec64ed84cb2b0a292b722fef4537e831c02c
Harshit-20 <hchoubey97@gmail.com>
2017-10-10 03:24:36 +0530

    Creates FinalRegistrations table

--- FusionIIIT/applications/academic_procedures/models.py
+++ FusionIIIT/applications/academic_procedures/models.py
@@ -1,1 +1,31 @@
-# from django.db import models
+from django.db import models
+from applications.globals.models import Student, ExtraInfo
+
+
+class Constants:
+    SEM_CHOICES = (
+        ('1', '1')
+        ('2', '2')
+        ('3', '3')
+        ('4', '4')
+        ('5', '5')
+        ('6', '6')
+        ('7', '7')
+        ('8', '8')
+    )
+
+
+class FinalRegistrations(models.Model):
+    reg_id = models.ForeignKey(ExtraInfo)
+    sem = models.IntegerField(max_length=1, choices=Constants.SEM_CHOICES)
+    student_id = models.ForeignKey(Student)
+    registration = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+
+
+
+
