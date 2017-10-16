cdccc6c9a5b3c0286fa07ee16c8a01bbb290413a
mshirisha <shireesha.muthineni@gmail.com>
2017-10-16 17:20:08 +0530

    models.py

--- FusionIIIT/applications/scholarships/models.py
+++ FusionIIIT/applications/scholarships/models.py
@@ -5,20 +5,23 @@ from django.db import models
 from applications.academic_information.models import Student
 # Create your models here
 
-class Mcm(models.Model):
-    mcm_id              = models.IntegerField(primary_key=True)
-    student_id         = models.ForeignKeyField(Student, on_delete=models.CASCADE)
-    income_total        = models.IntegerField()
-    income_file         = models.FileField(max_length=10)
-    banck_acc           = models.CharField(max_length=15)
-    brother_name        = models.CharField(max_length=30)
-    brother_occupation  = models.TextField(max_length=100)
-    sister_name         = models.CharField(max_length=30)
-    sister_occupation   = models.TextField(max_length=100)
-    income_father       = models.IntegerField()
-    income_mother       = models.IntegerField()
 
-    FATHER_OCC_CHOICES  = (
+class Constants:
+    MOTHER_OCC_CHOICES  = (
+                         ('EMPLOYED','EMPLOYED'),
+                         ('HOUSE_WIFE','HOUSE_WIFE')
+    )
+    HOUSE_TYPE_CHOICES =(
+                        ('RENTED','RENTED'),
+                        ('OWNED','OWNED')
+    )
+    STATUS_CHOICES  = (
+     
+        ('COMPLETE','COMPLETE'),
+        ('INCOMPLETE','INCOMPLETE')
+    )
+
+     FATHER_OCC_CHOICES  = (
             ( 'Service',(
                         ('government','Government'),
                         ('private','Private'),
@@ -31,47 +34,12 @@ class Mcm(models.Model):
                          ('consultant','Consultant'),
                         )
             ),
-            ('pensioners','Pensioners'),
+            ('pensioners','Pensioners')
+            
     )
-    father_occ_choice=models.CharField(max_length=10,choices=FATHER_OCC_CHOICES)
-    father_occ          = models.TextField(max_length=100)
-    EMPLOYED='EMP'
     
-    HOUSE_WIFE='HW'
-    MOTHER_OCC_CHOICES  =(
-                         (EMPLOYED,'employed'),
-                         (HOUSE_WIFE,'house-wife'),
-    )
-    mother_occ_choice=models.CharField(max_length=4,choices=MOTHER_OCC_CHOICES)
-    mother_occ         = models.TextField(max_length=100)
-    four_wheeler       = models.IntegerField()
-    two_wheeler        = models.IntegerField()
-    RENTED='rent'
-    OWNES='own'
-    HOUSE_TYPE_CHOICES =(
-                        (RENTED,'rented'),
-                        (OWNED,'owned'),
-    )
-    house_type         = models.CharField(max_length=5,choices=HOUSE_TYPE_CHOICES)
-    house_area         = models.IntegerField()
-    sSchool_10          = models.CharField(max_length=40)
-    school_10_fee      = models.IntegerField()
-    school_12          = models.CharField(max_length=40)
-    school_12_fee      = models.IntegerField()
-    father_declation   = models.FileField(null=True, blank=True)
-    affidavit          = models.FileField(null=True, blank=True)
-    
-    COMPLETE='CMP'
-    INCOMPLETE='ICMP'
-    STATUS_CHOICES=(
-        (COMPLETE,'complete'),
-        (INCOMPLETE,'incomplete'),
-    )
-    status            = models.CharField(max_length=4,choices=STATUS_CHOICES)
-    status_check      = models.BooleanField(default=False)
-
-class Constants:
-    time = (
+    time    =  (
+        ('0','12 Midnight'),
         ('1am','1'),
         ('2am','2'),
         ('3am','3'),
@@ -105,41 +73,88 @@ class Constants:
         ('PG1','PG1'),
         ('PG2','PG2')
     )
+class Mcm(models.Model):
+    mcm_id              = models.IntegerField()
+    student_id          = models.ForeignKeyField(Student, on_delete=models.CASCADE)
+    income_total        = models.IntegerField()
+    income_file         = models.FileField(max_length=10)
+    bank_acc_name       = models.TextField(max_length=50)
+    banck_acc_no        = models.IntegerField()
+    brother_name        = models.CharField(max_length=30)
+    brother_occupation  = models.TextField(max_length=100)
+    sister_name         = models.CharField(max_length=30)
+    sister_occupation   = models.TextField(max_length=100)
+    income_father       = models.IntegerField()
+    income_mother       = models.IntegerField()
+
+   
+    father_occ_choice   = models.CharField(max_length=10,choices=Constants.FATHER_OCC_CHOICES)
+    father_occ          = models.TextField(max_length=100)
+
+   
+    mother_occ_choice  = models.CharField(max_length=10,choices=Constants.MOTHER_OCC_CHOICES)
+    mother_occ         = models.TextField(max_length=100)
+    four_wheeler       = models.IntegerField()
+    four_wheeler_des   = models.TextField(max_length=30)
+
+    two_wheeler        = models.IntegerField()
+    two_wheeler_des    = models.TextField(max_length=30)
+   
+    house_type         = models.CharField(max_length=5,choices=Constants.HOUSE_TYPE_CHOICES)
+    house_area         = models.IntegerField()
+    school_10          = models.CharField(max_length=40)
+    school_10_fee      = models.IntegerField()
+    school_12          = models.CharField(max_length=40)
+    school_12_fee      = models.IntegerField()
+    father_declation   = models.FileField(null=True, blank=True)
+    affidavit          = models.FileField(null=True, blank=True)
+    
+   
+    
+    status            = models.CharField(max_length=4,choices=Constants.STATUS_CHOICES)
+    status_check      = models.BooleanField(default=False)
+
+     class Meta:
+        db_table       = 'Mcm'
+        
+    def __str__(self):
+        return self.mcm_id
+
 
 
 
 
 class Award_and_scholarship(models.Model):
-    award_id = models.IntegerField(primary_key=True)
-    award_name = models.CharField(max_length=50, default='') 
-    catalog = models.TextField()
+    award_id           = models.IntegerField()
+    award_name         = models.CharField(max_length=1000, default='') 
+    catalog            = models.TextField()
 
     class Meta:
-        db_table = 'Award_and_scholarship'
+        db_table       = 'Award_and_scholarship'
         
     def __str__(self):
         return str(self.award_id)
 
 class Previous_winner(models.Model):
-    previous_id = models.IntegerField(primary_key=True)
-    student_id = models.ForeignKey(Student)
-    award_id = models.ForeignKey(Award_and_scholarship)
+    previous_id        = models.IntegerField(primary_key=True)
+    student_id         = models.ForeignKey(Student)
+    award_id           = models.ForeignKey(Award_and_scholarship)
     class Meta:
-        db_table = 'Previous_winner'
+        db_table       = 'Previous_winner'
         
     def __str__(self):
         return self.previous_id
 
 class Release(models.Model):
-    release_id = models.IntegerField(primary_key=True)
-    award_id = models.ForeignKey(Award_and_scholarship)
-    startdate = models.DateTimeField(auto_now=False, auto_now_add=True)
-    enddate = models.DateTimeField(auto_now=False, auto_now_add=True)
-    venu = models.CharField(max_length=50)
-    time = models.CharField(max_length=10, choices=Constants.time)
-    batch = models.CharField(max_length=10, choices=Constants.batch)
+    release_id        = models.IntegerField()
+    award_id          = models.ForeignKey(Award_and_scholarship)
+    startdate         = models.DateTimeField(("Start date"), default=datetime.date.today)
+    enddate           = models.DateTimeField(("End date"))
+    venu              = models.CharField(max_length=50)
+    time              = models.CharField(max_length=10, choices=Constants.time)
+    batch             = models.CharField(max_length=10, choices=Constants.batch)
     class Meta:
-        db_table = 'Release'
+        db_table      = 'Release'
         
     def __str__(self):
         return str(self.release_id)
