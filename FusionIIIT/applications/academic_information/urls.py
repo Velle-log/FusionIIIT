d8d08ec453107652f5be89a8f6b8d7d5570c9e75
narosen <1311naro@gmail.com>
2017-10-28 17:38:01 +0530

    Senate view so far

--- FusionIIIT/applications/academic_information/urls.py
+++ FusionIIIT/applications/academic_information/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from . import views
+from . import views 
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^input/$', views.test, name='input'),
 ]
--- FusionIIIT/applications/academic_information/views.py
+++ FusionIIIT/applications/academic_information/views.py
@@ -1,10 +1,33 @@
 # from django.shortcuts import render
 
 from django.shortcuts import render
+from .models import Student
+from applications.globals.models import ExtraInfo, Designation, DepartmentInfo
+from django.template.defaulttags import csrf_token
+from django.http import HttpResponse
 
 
 def homepage(request):
-    context = {}
 
+    s = Designation.objects.get(name = 'senate')
+    try:
+        senates = ExtraInfo.objects.filter(designation = s)
+    except:
+        senates = ""
+        pass
+        
+    context = {
+         'senates':senates,
+    }
+    
     return render(request, "ais/ais.html", context)
+
+def test(request):
+    s = Designation.objects.get(name = 'senate')
+    if request.method == 'POST':
+        extraInfo = ExtraInfo.objects.get(id = request.POST['Roll Number'])
+        extraInfo.designation.add(s)
+        extraInfo.save()
+    return HttpResponse("Data Inputed")
+
 
