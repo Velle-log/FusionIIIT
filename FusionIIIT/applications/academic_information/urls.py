dfb63c0ca5e524675a314134bfccc9b27f8fef0b
sirisha-rambha <32480684+sirisha-rambha@users.noreply.github.com>
2017-11-04 16:01:34 +0530

    update

--- FusionIIIT/applications/academic_information/urls.py
+++ FusionIIIT/applications/academic_information/urls.py
@@ -6,6 +6,8 @@ app_name = 'academic_information'
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^senate/$', views.senate, name='senate'),
-    url(r'^input/$', views.test, name='input'),
     url(r'^delete/$', views.delete, name='delete'),
+    url(r'^edit_convenor/$', views.edit_convenor, name='edit_convenor'),
+    url(r'^delete1/$', views.delete1, name='delete1'),
+    url(r'^delete2/$', views.delete2, name='delete2'),
 ]
--- FusionIIIT/applications/academic_information/views.py
+++ FusionIIIT/applications/academic_information/views.py
@@ -13,17 +13,25 @@ import json
 def homepage(request):
     try:
         s = Designation.objects.get(name = 'senate')
+        v = Designation.objects.get(name = 'Convenor')
+        t = Designation.objects.get(name = 'Co Convenor')   
         senates = ExtraInfo.objects.filter(designation = s)
+        Convenor = ExtraInfo.objects.filter(designation = v)
+        CoConvenor = ExtraInfo.objects.filter(designation = t)
         students = Student.objects.filter(id__in = senates)
         
     except:
         senates = ""
         students = ""
+        Convenor = ""
+        CoConvenor = ""
         pass
         
     context = {
          'senates':senates,
          'students':students,
+         'Convenor':Convenor,
+         'CoConvenor':CoConvenor,
     }    
     return render(request, "ais/ais.html", context)
 
@@ -52,3 +60,30 @@ def senate(request):
     else:        
         data = {}
         return JsonResponse(data)
+
+def edit_convenor(request):
+    s = Designation.objects.get(name = 'Convenor')
+    p = Designation.objects.get(name = 'Co Convenor')
+    if request.method == 'POST':
+        extraInfo = ExtraInfo.objects.get(id = request.POST["Roll Number"])
+        result = request.POST["Designation"]
+        if result == "Convenor":
+           extraInfo.designation.add(s)
+           extraInfo.save()
+        else:
+           extraInfo.designation.add(p)
+           extraInfo.save()
+    return HttpResponse("Data Inputed")
+
+def delete1(request):
+   s = Designation.objects.get(name = "Convenor")
+   student = ExtraInfo.objects.get(id = request.POST["delete"])
+   student.designation.remove(s)
+   return HttpResponse("Deleted")
+
+def delete2(request):
+   s = Designation.objects.get(name = "CoConvenor")
+   student = ExtraInfo.objects.get(id = request.POST["delete"])
+   student.designation.remove(s)
+   return HttpResponse("Deleted")
+
