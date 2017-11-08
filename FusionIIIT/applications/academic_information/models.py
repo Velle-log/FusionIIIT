5c5fe67e78cffd9f8a770f072408ed06edd7c090
sushmithad14 <32462574+sushmithad14@users.noreply.github.com>
2017-11-08 19:33:25 +0530

    update

--- FusionIIIT/applications/academic_information/models.py
+++ FusionIIIT/applications/academic_information/models.py
@@ -38,7 +38,6 @@ class Student(models.Model):
     mother_name = models.CharField(max_length=40, default='')
     hall_no = models.IntegerField(default=1)
     room_no = models.CharField(max_length=10, blank=True, null=True)
-    category = models.CharField(max_length=10, choices=Constants.CATEGORY),
 
     def __str__(self):
         return str(self.id)
