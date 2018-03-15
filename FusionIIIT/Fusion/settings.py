a28a003c28eacd85774554779f927e04e83083bf
Gaurav Nayak <gauravnayak@iiitdmj.ac.in>
2018-03-15 21:49:15 +0530

    Update settings.py

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -153,7 +153,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'fusion',
             'USER': 'root',
-            'PASSWORD': 'alohomora',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         },
